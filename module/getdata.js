58aca9a28db638fe6452f4b7e6918ab0f75c86b7
09jetsalit <rovesea1@gmail.com>
2024-02-11 20:24:04 +0700

    asdasd

--- module/getdata.js
+++ module/getdata.js
@@ -2,6 +2,7 @@ import databaseClient from "../services/database.mjs";
 
 const getdata = async (req, res) => {
     let body = req.body;
+    console.log(body);
   const data = await databaseClient
     .db()
     .collection("members")
